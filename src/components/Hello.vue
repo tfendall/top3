3edd69d2110eb27059cdaf5eb1e816ff2b0e0a26
Troy Fendall <troy@pro.com>
2017-02-20 21:25:07 -0800

    Installation of Vue webpack template + material design and vue-mdl

---
+++ src/components/Hello.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>What are your top 3 types of Colgate brand toothpaste?</h1>
+    <ul class="list">
+      <li v-for="item in items">
+        <mdl-textfield :floating-label="item.label" v-model="item.value"></mdl-textfield>
+      </li>
+    </ul>
+    <mdl-button>Submit</mdl-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      items: [
+        { label: 'One' },
+        { label: 'Two' },
+        { label: 'Three' },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  max-width: 800px;
+  margin: 0  auto;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import VueMdl from 'vue-mdl';
+import App from './App';
+import router from './router';
+
+Vue.use(VueMdl);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App },
+});
